9720466470fada9accfdcde2ef23fbaba69a7b67
Almaz <almazbek.muhtarov@gmail.com>
2022-05-06 16:34:46 +0600

    added exit button

--- lib/screens/signe_out_page.dart
+++ lib/screens/signe_out_page.dart
@@ -14,40 +14,43 @@ class _SigneOutPageState extends State<SigneOutPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Center(
-            child: Text('Signe in as:\n${user!.email!}',   
-            textAlign: TextAlign.center,
-            style: const TextStyle(
-                          color: Colors.blueGrey,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 22),),
+            child: Text(
+              'Signe in as:\n${user!.email!}',
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                  color: Colors.blueGrey,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22),
+            ),
           ),
-          const SizedBox(height: 20,),
-                   Container(
-                padding: const EdgeInsets.all(10.0),
-                margin: const EdgeInsets.symmetric(horizontal: 20.0),
-                width: 120,
-                decoration: BoxDecoration(
-                    color: Colors.green[300],
-                    borderRadius: BorderRadius.circular(12.0)),
-                child:  Center(
-                  child: GestureDetector(
-                    onTap: () {
-                     FirebaseAuth.instance.signOut();
-                    },
-                    child: const Text(
-                      'Sign out',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18),
-                    ),
-                  ),
+          const SizedBox(
+            height: 20,
+          ),
+          Container(
+            padding: const EdgeInsets.all(10.0),
+            margin: const EdgeInsets.symmetric(horizontal: 20.0),
+            width: 120,
+            decoration: BoxDecoration(
+                color: Colors.green[300],
+                borderRadius: BorderRadius.circular(12.0)),
+            child: Center(
+              child: GestureDetector(
+                onTap: () {
+                  FirebaseAuth.instance.signOut();
+                },
+                child: const Text(
+                  'Sign out',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18),
                 ),
               ),
+            ),
+          ),
         ],
       ),
     );
